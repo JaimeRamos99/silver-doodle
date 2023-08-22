45d3bf59aeddb8ea1f77c14a8babdaee30f540c2
Jaime Ramos <jrossetes@gmail.com>
2023-08-22 18:53:24 -0500

    fix

--- src/controllers/battle.controller.ts
+++ src/controllers/battle.controller.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { Battle } from '../models';
+import { Battle, Monster } from '../models';
 import { Id } from 'objection';
+import { calculateFirstAttacker } from '../services';
 
 export const get = async (req: Request, res: Response): Promise<Response> => {
   const id: Id = req.params.id;
@@ -26,8 +27,32 @@ const remove = async (req: Request, res: Response): Promise<Response> => {
   return res.sendStatus(StatusCodes.NO_CONTENT);
 }
 
+const startBattle = async (req: Request, res: Response): Promise<Response> => {
+  const { monsterAId, monsterBId } = req.body;
+  if (!monsterAId || !monsterBId) {
+    return res.sendStatus(StatusCodes.BAD_REQUEST);
+  }
+
+  const monsterA = await Monster.query().findById(monsterAId);
+  if (!monsterA) {
+    return res.sendStatus(StatusCodes.NOT_FOUND);
+  }
+
+  const monsterB = await Monster.query().findById(monsterBId);
+  if (!monsterB) {
+    return res.sendStatus(StatusCodes.NOT_FOUND);
+  }
+
+  const currentAttacker = calculateFirstAttacker(monsterA, monsterB);
+  while(monsterA.hp > 0 || monsterB.hp > 0) {
+
+  }
+  return res;
+}
+
 export const BattleController = {
   get,
   list,
-  remove
+  remove,
+  startBattle
 };
